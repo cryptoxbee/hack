43a6677af141991994965dddd7690f02320aa3e0
onursenturky <onursenturk2003@gmail.com>
2025-02-06 19:32:58 +0300

    yeni frontend

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,17 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          500: '#8B5CF6', // Ana mor renk
-          600: '#7C3AED',
-          700: '#6D28D9',
-        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
-}
-
-export default config;
+} satisfies Config;
